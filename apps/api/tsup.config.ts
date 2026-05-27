3fba83ff6652609b4e28834581eb22df85275afe
ravi8555 <ravi8555@gmail.com>
2026-05-27 12:31:47 +0530

    changes_tsup.config

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -1,14 +1,46 @@
+// import { defineConfig } from "tsup";
+
+// export default defineConfig({
+//   entry: ["./src/index.ts"],
+//   noExternal: ["@teachyst"], // transpile packages starting with `@teachyst` and their dependencies
+//   splitting: false,
+//   bundle: true,
+//   outDir: "./dist",
+//   clean: true,
+//   env: { IS_SERVER_BUILD: "true" },
+//   loader: { ".json": "copy" },
+//   minify: true,
+//   sourcemap: false,
+// });
+
+
 import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["./src/index.ts"],
-  noExternal: ["@teachyst"], // transpile packages starting with `@teachyst` and their dependencies
-  splitting: false,
+
   bundle: true,
+  splitting: false,
   outDir: "./dist",
   clean: true,
-  env: { IS_SERVER_BUILD: "true" },
-  loader: { ".json": "copy" },
+
+  noExternal: [
+    "@repo/logger",
+    "@repo/trpc",
+    "@repo/services",
+    "@repo/database",
+  ],
+
+  env: {
+    IS_SERVER_BUILD: "true",
+  },
+
+  loader: {
+    ".json": "copy",
+  },
+
   minify: true,
   sourcemap: false,
+  target: "node20",
+  format: ["cjs"],
 });
