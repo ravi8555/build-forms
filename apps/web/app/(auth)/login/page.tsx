e22703446b606953c9e148b4030e27abb73cd815
ravi8555 <ravi8555@gmail.com>
2026-07-02 22:40:16 +0530

    fixed prop issue

--- apps/web/app/(auth)/login/page.tsx
+++ apps/web/app/(auth)/login/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
           <div className="flex flex-col gap-4 p-6 md:p-10">               
             <div className="flex flex-1 items-center justify-center">              
               <div className="w-full max-w-sm">                
-                <LoginForm />
+                {/* <LoginForm /> */}
               </div>
             </div>
           </div>
